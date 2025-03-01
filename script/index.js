9e50ee1613506d2e739a3829e240b23fc73c9af8
faisalbari193 <faisalbari193@gmail.com>
2025-03-01 11:51:19 +0600

    added new code

--- script/index.js
+++ script/index.js
@@ -37,3 +37,15 @@ for (let button of buttons) {
     });
 }
 
+const button = document.getElementById('color-btn');
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+button.addEventListener('click', function() {
+  document.body.style.backgroundColor = getRandomColor();
+});
